525629126ad269c5024b1a0597ff9f4c45480438
AbdelmoneamIsmael <136120129+AbdelmoneamIsmael@users.noreply.github.com>
2025-10-31 05:09:34 +0200

    add back arrorw in home

--- lib/features/home/domain/src/categories.dart
+++ lib/features/home/domain/src/categories.dart
@@ -1,5 +1,4 @@
 import 'package:turbo_waiter/features/home/domain/entities/categories_entity.dart';
-import 'package:turbo_waiter/features/home/presentation/cubit/home_cubit.dart';
 
 final categories = [
   const CategoryItemEntity(
@@ -53,7 +52,7 @@ final categories = [
   ),
   const CategoryItemEntity(
     id: '8',
-    name: "🍮 الحلويات العراقية",
+    name: " الحلويات العراقية",
     imagePath:
         "https://img.youm7.com/ArticleImgs/2017/7/30/87891-%D8%AD%D9%84%D9%88%D9%8A%D8%A7%D8%AA-%D8%B4%D8%B1%D9%82%D9%8A%D8%A9-%D9%80-%D8%A7%D9%84%D8%AF%D8%A7%D9%84%D8%B7%D9%8A.jpg",
     color: 'orange3',
@@ -61,14 +60,14 @@ final categories = [
 
   const CategoryItemEntity(
     id: '10',
-    name: "🧃 المشروبات بارده",
+    name: " المشروبات بارده",
     imagePath:
         "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3dM6HsEHzEX-ErEptw_q9ou9JFZ74GUxs1Q&s",
     color: 'orange3',
   ),
   const CategoryItemEntity(
     id: '11',
-    name: "🍟 المقبلات الساخنة",
+    name: " المقبلات الساخنة",
     imagePath:
         "https://egf8xpxjy4g.exactdn.com/wp-content/uploads/2022/10/%D9%82%D9%87%D9%88%D8%A9-%D8%B9%D8%A7%D8%B1%D9%81-%D8%A7%D9%88%D8%BA%D9%84%D9%88-1024x707.webp?lossy=1&ssl=1",
     color: 'orange4',
--- lib/features/home/presentation/pages/home_screen.dart
+++ lib/features/home/presentation/pages/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:turbo_waiter/core/helpers/extensions.dart';
+import 'package:turbo_waiter/core/theming/styles.dart';
 import 'package:turbo_waiter/core/widgets/screen_wrapper.dart';
 import '../cubit/home_cubit.dart';
 import '../widgets/order_panel_widget.dart';
@@ -14,12 +16,37 @@ class HomeScreen extends StatelessWidget {
       create: (context) => HomeCubit(),
       child: ScreenWrapper(
         body: SafeArea(
-          child: Row(
+          child: Column(
+            spacing: 20,
             children: [
-              // Left Column - Order Panel
-              Expanded(flex: 2, child: const OrderPanelWidget()),
-              // Right Column - Menu Grid
-              Expanded(flex: 3, child: const MenuSectionWidget()),
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      context.pop();
+                    },
+
+                    icon: Icon(Icons.arrow_back_ios_new, color: Colors.white),
+                  ),
+                  Expanded(
+                    child: Text(
+                      'تسجيل الطلبات',
+                      style: TextStyles.font16WhiteBold,
+                    ),
+                  ),
+                ],
+              ),
+
+              Expanded(
+                child: Row(
+                  children: [
+                    // Left Column - Order Panel
+                    Expanded(flex: 2, child: const OrderPanelWidget()),
+                    // Right Column - Menu Grid
+                    Expanded(flex: 3, child: const MenuSectionWidget()),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
